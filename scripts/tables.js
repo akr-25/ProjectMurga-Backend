ff07e01150f646e60c3b83e119b3d869c021016a
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2022-01-23 03:18:56 +0530

    create tables script added

---
+++ scripts/tables.js
@@ -0,0 +1,13 @@
+const { sequelize } = require('../models');
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected');
+        await sequelize.sync({ force: true });
+        console.log("Tables synced");
+    }
+    catch (err) {
+        console.log(err);
+    }
+})()
