c6a24ced830d389df6211a9c97c3f3ca336480d6
Eeshaan Dutta <esnd18@gmail.com>
2022-06-04 11:30:51 +0530

    fetch and add fix

--- routes/add.js
+++ routes/add.js
@@ -10,44 +10,13 @@ const { where } = require('sequelize');
 const Op = Sequelize.Op;
 
 
-
-router.get('/', (req, res) => {
-    const data = {
-        request_id: "1",
-        applicant_id: "1",
-        approval: "N",
-        type_of_unit: "AA",
-        req_no_of_units: 1,
-        order_type: "D"
-
-    }
-    let { request_id,
-        applicant_id,
-        approval,
-        type_of_unit,
-        req_no_of_units,
-        order_type
-    } = data;
-    const req1 = Request.create({
-        request_id,
-        applicant_id,
-        approval,
-        type_of_unit,
-        req_no_of_units,
-        order_type
-
-    })
-        .then(gig => res.redirect('/'))
-        .catch(e => console.log(e));
-});
-
-
 router.post('/user', async (req, res) => {
     // { user_id, first_name, last_name, contact_no, email, password } = req.body;
     try {
         const user = await User.create(req.body); 
         // console.log(user) 
-        return res.json(user);
+        // return res.json(user);
+        res.end()
     }
     catch (err) {
         console.log(err);
@@ -55,7 +24,7 @@ router.post('/user', async (req, res) => {
     }
 })
 
-router.post('/req', async (req, res) => {
+router.post('/request', async (req, res) => {
     // const { user_id } = req.body;
     // const {applicant_id} = req.body;  
     try {
@@ -69,13 +38,27 @@ router.post('/req', async (req, res) => {
     }
 })
 
+router.post('/transaction', async(req, res) => {
+    // { transaction_id, order_id, unit_id, no_of_units, rate_per_unit}
+    try {
+        const request = await Request.findOne({where: req.body.order_id})
+        const transaction = await request.createTransaction(req.body)
+        res.end() 
+    } 
+    catch(err){
+        console.log(err)
+        return res.status(500).json(err) 
+    }  
+})
+
 router.post('/feedConsumption', async (req, res) =>{
     //const {date, unit_id, rate, cost_per_gram} = req.body; 
 
     try {
-        const batch = await Batch.findOne({where : {batch_id:req.body.unit_id}})
+        const batch = await Batch.findOne({where : {batch_id: req.body.unit_id}})
         const feedConsumption = await batch.createFeedConsumptionLog(req.body)
-        return res.json(feedConsumption); 
+        // return res.json(feedConsumption); 
+        res.end()
     }
     catch(err){
         console.log(err)
@@ -87,40 +70,46 @@ router.post('/priceLog', async (req, res) =>{
    // const {date, unit_id, price_per_unit} = req.body; 
 
     try {
-        const batch = await Batch.findOne({where : {batch_id:req.body.unit_id}})
+        const batch = await Batch.findOne({where : {batch_id: req.body.unit_id}})
         const priceLog = await batch.createPriceLog(req.body)
-        return res.json(priceLog); 
+        // return res.json(priceLog); 
+        res.end() 
+        //? frontend pe kuch send krna hota hai kya? 
     }
     catch(err){
         console.log(err)
         return res.status(500).json(err) 
     }
 })
+
 router.post('/balanceLogs', async (req, res) =>{
     // const {date, unit_id, net_balance_type1,net_balance_type2,type_of_change} = req.body; 
  
      try {
          const batch = await Batch.findOne({where : {batch_id:req.body.unit_id}})
          const balanceLog = await batch.createBalanceLog(req.body)
-         return res.json(balanceLog); 
+        //  return res.json(balanceLog); 
+        res.end()
      }
      catch(err){
          console.log(err)
          return res.status(500).json(err) 
      }
  })
-//  router.post('/createBatch', async (req, res) =>{
-//     // const {date, unit_id, net_balance_type1,net_balance_type2,type_of_change} = req.body; 
+
+ router.post('/batch', async (req, res) =>{
+    // const {batch_id} = req.body; 
  
-//      try {
-//          const batch = await Batch.create(req.body)
-//         // const balanceLog = await batch.createBatch(req.body)
-//          return res.json(batch); 
-//      }
-//      catch(err){
-//          console.log(err)
-//          return res.status(500).json(err) 
-//      }
-//  })
+     try {
+         const batch = await Batch.create(req.body)
+        //  return res.json(batch); 
+        res.end(); 
+
+     }
+     catch(err){
+         console.log(err)
+         return res.status(500).json(err) 
+     }
+ })
 
 module.exports = router;
--- routes/fetch.js
+++ routes/fetch.js
@@ -5,12 +5,17 @@ const db = require('../database/mysql');
 
 const Sequelize = require('sequelize');
 const { sequelize } = require('../models');
-const {Request, User, Transaction } = require('../models');
+const {Request, User, Transaction, Batch, PriceLog} = require('../models');
 const Op = Sequelize.Op;
 
 router.get('/request' , async(req, res) => {
+    const {user_id} = req.body; 
+
     try{
-        const request  = await Request.findAll()
+        const request  = await User.findOne({
+            where: {user_id : user_id}, 
+            include: Request
+        })
         res.send(request)
     }
     catch(err){
@@ -19,9 +24,52 @@ router.get('/request' , async(req, res) => {
     }
 })
 
-router.get('/transaction', async(req, res) =>{
-    Transaction.findAll()
-    .then()
+router.get('/priceLogs', async(req, res) =>{
+    //! untested code below, remove comment after testing
+
+    const {start} = req.body; 
+
+    try{
+        const pricelogs = await PriceLog.findAll({
+            model: PriceLog, 
+            where: {
+                date : {
+                    [Op.gte] : start 
+                    // all pricelogs such that pricelogs.date >= start
+                } 
+            }
+        })
+
+        res.send(pricelogs)
+    }
+    catch(err){
+        console.log(err)
+        return res.status(500).json(err) 
+    }
+})
+
+router.get('/feedConsumptionLogs', async(req, res) =>{
+    //! untested code below, remove comment after testing
+
+    const {start} = req.body; 
+
+    try{
+        const pricelogs = await PriceLog.findAll({
+            model: PriceLog, 
+            where: {
+                date : {
+                    [Op.gte] : start 
+                    // all pricelogs such that pricelogs.date >= start
+                } 
+            }
+        })
+        
+        res.send(pricelogs)
+    }
+    catch(err){
+        console.log(err)
+        return res.status(500).json(err) 
+    }
 })
 
 module.exports = router;
