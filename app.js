487ed9266e3f117901ae84073861a6e570e93c21
Kunal Kartik <kunalkartik02@gmail.com>
2022-06-04 11:47:54 +0530

    addControllers.js added

--- app.js
+++ app.js
@@ -9,50 +9,35 @@ const passport = require('passport')
 // importing this for its side effects only, which is to configure passport
 require("./passportConfig/passport.js");
 
-const {redisSession} = require('./database/redisConfig');
+const { redisSession } = require('./database/redisConfig');
 
 const app = express();
 app.set('view engine', 'ejs');
 
 //Middleware
 app.use(redisSession);
-<<<<<<< HEAD
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-=======
->>>>>>> c87249011b5caa616b5278245a9a92974d1eced8
 // init passport on every route call
-app.use(passport.initialize()) 
+app.use(passport.initialize())
 //allow passport to use "express-session"
 app.use(passport.session())
 
 app.use(morgan("dev"));
-<<<<<<< HEAD
-
-=======
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true
 }));
->>>>>>> c87249011b5caa616b5278245a9a92974d1eced8
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-<<<<<<< HEAD
 // Routes
 app.use("/auth", require('./routes/auth'));
 app.use("/dashboard", require('./routes/dashboard'));
 app.use("/", require('./routes/index'));
 app.use("/add", require('./routes/add'));
-app.use("/pre", require('./routes/get_req'));
+app.use("/pre", require('./routes/fetch'));
 
 
-=======
-
-// Routes
-app.use("/auth", require('./routes/auth'));
-app.use("/dashboard", require('./routes/dashboard'));
->>>>>>> c87249011b5caa616b5278245a9a92974d1eced8
 
 
 const server = app.listen(process.env.PORT || 3001, async () => {
@@ -61,7 +46,7 @@ const server = app.listen(process.env.PORT || 3001, async () => {
     await sequelize.authenticate();
     await sequelize.sync();
     console.log("Database connected");
-  } catch (err) {
+  } catch (err) { 
     console.log(err);
   }
 });
--- routes/add.js
+++ routes/add.js
@@ -3,124 +3,17 @@ const router = express.Router();
 
 const db = require('../database/mysql');
 
-const Sequelize = require('sequelize');
-const { sequelize } = require('../models');
 const { Request, User, FeedConsumptionLog, PriceLog ,Batch} = require('../models');
 const { where } = require('sequelize');
+const {addUser,addRequest,addBalanceLog,addFeedConsumption,addPriceLog,addBatch,addTransanction} = require('../controllers/addController.js')
 const Op = Sequelize.Op;
 
-
-
-router.get('/', (req, res) => {
-    const data = {
-        request_id: "1",
-        applicant_id: "1",
-        approval: "N",
-        type_of_unit: "AA",
-        req_no_of_units: 1,
-        order_type: "D"
-
-    }
-    let { request_id,
-        applicant_id,
-        approval,
-        type_of_unit,
-        req_no_of_units,
-        order_type
-    } = data;
-    const req1 = Request.create({
-        request_id,
-        applicant_id,
-        approval,
-        type_of_unit,
-        req_no_of_units,
-        order_type
-
-    })
-        .then(gig => res.redirect('/'))
-        .catch(e => console.log(e));
-});
-
-
-router.post('/user', async (req, res) => {
-    // { user_id, first_name, last_name, contact_no, email, password } = req.body;
-    try {
-        const user = await User.create(req.body); 
-        // console.log(user) 
-        return res.json(user);
-    }
-    catch (err) {
-        console.log(err);
-        return res.status(500).json(err)
-    }
-})
-
-router.post('/req', async (req, res) => {
-    // const { user_id } = req.body;
-    // const {applicant_id} = req.body;  
-    try {
-        const user = await User.findOne({where: {user_id : req.body.applicant_id}});
-        const request = await user.createRequest(req.body); 
-        res.end(); 
-    }
-    catch (err) {
-        console.log(err);
-        return res.status(500).json(err)
-    }
-})
-
-router.post('/feedConsumption', async (req, res) =>{
-    //const {date, unit_id, rate, cost_per_gram} = req.body; 
-
-    try {
-        const batch = await Batch.findOne({where : {batch_id:req.body.unit_id}})
-        const feedConsumption = await batch.createFeedConsumptionLog(req.body)
-        return res.json(feedConsumption); 
-    }
-    catch(err){
-        console.log(err)
-        return res.status(500).json(err) 
-    }
-})
-
-router.post('/priceLog', async (req, res) =>{
-   // const {date, unit_id, price_per_unit} = req.body; 
-
-    try {
-        const batch = await Batch.findOne({where : {batch_id:req.body.unit_id}})
-        const priceLog = await batch.createPriceLog(req.body)
-        return res.json(priceLog); 
-    }
-    catch(err){
-        console.log(err)
-        return res.status(500).json(err) 
-    }
-})
-router.post('/balanceLogs', async (req, res) =>{
-    // const {date, unit_id, net_balance_type1,net_balance_type2,type_of_change} = req.body; 
- 
-     try {
-         const batch = await Batch.findOne({where : {batch_id:req.body.unit_id}})
-         const balanceLog = await batch.createBalanceLog(req.body)
-         return res.json(balanceLog); 
-     }
-     catch(err){
-         console.log(err)
-         return res.status(500).json(err) 
-     }
- })
-//  router.post('/createBatch', async (req, res) =>{
-//     // const {date, unit_id, net_balance_type1,net_balance_type2,type_of_change} = req.body; 
- 
-//      try {
-//          const batch = await Batch.create(req.body)
-//         // const balanceLog = await batch.createBatch(req.body)
-//          return res.json(batch); 
-//      }
-//      catch(err){
-//          console.log(err)
-//          return res.status(500).json(err) 
-//      }
-//  })
+router.post('/transaction', addTransanction)
+router.post('/user', addUser)
+router.post('/req', addRequest)
+router.post('/feedConsumption', addFeedConsumption)
+router.post('/priceLog',addPriceLog)
+router.post('/balanceLogs', addBalanceLog)
+router.post('/createBatch', addBatch)
 
 module.exports = router;
