c99fbd7002d79d9a5c40c74593830d8b03509b5f
Kunal Kartik <kunalkartik02@gmail.com>
2022-06-30 11:49:50 +0530

    CSV logic done

--- scripts/makecsv.js
+++ scripts/makecsv.js
@@ -1,23 +1,82 @@
 const { promises: fs } = require("fs");
 const createCSVwriter = require("csv-writer").createObjectCsvWriter;
 
-const csvWriter = createCSVwriter({
-  path: "./batch.csv",
+const csvWriter1 = createCSVwriter({
+  path: "./egg.csv",
   header: [
     "date",
-    "net_balance_type1",
-    "net_balance_type2",
-    "type_of_change",
-    "rate",
-    "cost_per_gram",
-    "price_per_unit",
+    "unit_id",
+    "previous_balance_table",
+    "previous_balance_hatching",
+    "Produced_Table",
+    "Produced_Hatching",
+
+    "type_of_unit",
+    "Total_Table",
+    "Total_Hatching",
+    "Sale_Table",
+    "Sale_Hatching", //(units sold)
+    "Buyer_info",
+    "Sale_Rate",
+    "Amount",
+    "order_type",
   ].map((item) => ({ id: item, title: item })),
 });
 
+const csvWriter2 = createCSVwriter({
+  path: "./chick.csv",
+  header: [
+    "date",
+    "unit_id",
+    "previous_balance",
+    "Chick_Born",
+    "Mortality",
+    "Total",
+    "type_of_unit",
+    "Feed_Consumption_Rate",
+    "Feed_Cost",
+    "Total_Feed_Consumed", //automated
+    "Sale", //(units sold)
+    "Buyer_info",
+    "Sale_Rate",
+    "Amount",
+    "order_type",
+  ].map((item) => ({ id: item, title: item })),
+});
+const csvWriter3 = createCSVwriter({
+  path: "./grower.csv",
+  header: [
+    "date",
+    "unit_id",
+    "previous_balance_male",
+    "previous_balance_female",
+    "Recieved_Male",
+    "Recieved_Female",
+    "Transfer_to_grower_Male",
+    "Transfer_to_grower_Female", //(chick->gr)
+    "Mortality_Male",
+    "Mortality_Female",
+    "type_of_unit",
+    "Total_Male",
+    "Total_Female",
+    "Feed_Consumption_Rate",
+    "Feed_Cost",
+    "Total_Feed_Consumed", //automated
+    "Sale_Male",
+    "Sale_Female", //(units sold)
+    "Buyer_info",
+    "Sale_Rate",
+    "Amount",
+    "order_type",
+  ].map((item) => ({ id: item, title: item })),
+});
 module.exports = {
-  generateCSV: async function (finalData) {
+  generateCSV: async function (finalData, type) {
     try {
-      await csvWriter.writeRecords(finalData);
+      if (type == 1) await csvWriter1.writeRecords(finalData);
+      if (type == 2) await csvWriter2.writeRecords(finalData);
+
+      if (type == 3) await csvWriter3.writeRecords(finalData);
     } catch (e) {
       console.log(e);
     }
