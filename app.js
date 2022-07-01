8eaaef1f3d160e16dcac43cefa4df3eb83e166f3
Aman Kumar <aman.26262@gmail.com>
2022-07-01 21:51:35 +0530

    errorHandling added

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { sequelize } = require("./models");
 const passport = require("passport");
+const {errorHandler} = require('./errors/errorHandler');
 
 // importing this for its side effects only, which is to configure passport
 require("./passportConfig/passport.js");
 
 const { redisSession } = require("./database/redisConfig");
-const handleError = require("./middleware/handleError");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -45,27 +45,10 @@ app.use("/api/request", require("./routes/request"));
 
 app.use("/dashboard", require("./routes/dashboard"));
 app.use("/", require("./routes/index"));
-
+// app.use('*', error404 page);
 
 //* Error Handler
-app.use((err, req, res, next) => {
-  const new_err = {} 
-
-
-  if(err.name === "SequelizeForeignKeyConstraintError"){
-      new_err["message"] = `cannot find the specified ${err.fields[0]}`
-  }
-  else if(err.name === "SequelizeValidationError"){
-      new_err["type"] = `${err.errors[0].type} on ${err.errors[0].path}`
-      new_err["message"] = err.errors[0].message
-  }
-  else new_err = err
-  // console.log(err) 
-  //! auth controller errors are not handled yet
-  //! don't know if there are other possible sequelize errors? 
-
-  res.status(500).send({ error: new_err, message: "failure", data: null });
-});
+app.use(errorHandler);
 
 const server = app.listen(process.env.PORT || 3001, async () => {
   console.log(`Server is running on http://localhost:${server.address().port}`);
