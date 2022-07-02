97cda6528ed15bb72f3b1d42d896d3f481b14ee8
Eeshaan Dutta <83598773+Z1R0-byte@users.noreply.github.com>
2022-07-03 00:19:29 +0530

    cleanup

--- routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-let router = express.Router();
-const checkAuth = require("../middleware/checkAuth");
-
-router.get("/", checkAuth, (req, res) => {
-  res.render("dashboard", { name: req.user.first_name });
-});
-
-module.exports = router;
