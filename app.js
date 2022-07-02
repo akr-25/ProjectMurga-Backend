f372cfe37238f74b13a0d53a6a547bbbe58558e9
Eeshaan Dutta <83598773+Z1R0-byte@users.noreply.github.com>
2022-07-03 00:14:03 +0530

    we fucked up

--- app.js
+++ app.js
@@ -33,6 +33,42 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Load config
+dotenv.config({path:'./config/config.env'});
+
+// Passport
+require('./config/passport')(passport);
+
+// Views
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+// Sessions
+app.use(
+    session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  })
+)
+  
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//--------------------------------------------------
+
+// Routes
+app.use('/',require('./routes/index'));
+app.use('/auth',require('./routes/auth'));
+
+
 
 // Routes
 app.use("/auth", require("./routes/auth"));
